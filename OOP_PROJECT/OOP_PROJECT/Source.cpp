8e184cc2a8460bbff78e64742826e27bb00f11bd
Steli <161700044+Stela-Stoimenova@users.noreply.github.com>
2024-11-14 13:43:45 +0300

    Update Source.cpp
    I just copied the code from the other source.cpp

--- OOP_PROJECT/OOP_PROJECT/Source.cpp
+++ OOP_PROJECT/OOP_PROJECT/Source.cpp
@@ -1,21 +1,78 @@
 #include <iostream>
 #include <cstring>
 #include <string.h>
-#include <string>
+#include<string>
 
-int main() 
+using namespace std;
+
+class Input
 {
+private:
 	bool isRunning = true;
-	std::string userCommand;
+	string userCommand = "";
+	string delimiter = " ";
+	string* commandList = nullptr;
+	// int noCommands = 0;
 
-	while (isRunning) {
-		std::cout << std::endl << "Enter a command: ";
-		getline(std::cin, userCommand);
+public:
+	/*void addToCommandList(string word, int noCommands)
+	{
+		string* listCopy = new string[noCommands];
+		for(int i{};i<noCommands;++i)
+		{
+			listCopy[i] = this->commandList[i];
+		}
+		delete[] this->commandList;
+		for (int i{}; i < noCommands; ++i)
+		{
+			this->commandList[i] = listCopy[i];
+		}
+		this->commandList[noCommands] = word;
+		this->noCommands++;
+	}*/
+	void readInput()
+	{
+		while (isRunning) {
+			cout << endl << "Enter a command: ";
+			getline(cin, userCommand);
 
-		// Exiting the application
-		if (userCommand == std::string("0")) {
-			isRunning = false;
+			// Exiting the application
+			if (userCommand == string("0")) {
+				isRunning = false;
+			}
+			else
+			{
+				auto pos = userCommand.find(delimiter);
+				cout << "First word is: " << userCommand.substr(0, pos) << endl;
+				while (pos != string::npos) {
+					cout << userCommand.substr(0, pos) << " ";
+					userCommand.erase(0, pos + delimiter.length());
+					pos = userCommand.find(delimiter);
+					
+				}
+				pos = userCommand.find('\0');
+				cout << userCommand.substr(0, pos) << " ";
+				userCommand.erase(0, pos + delimiter.length());
+
+			}
 		}
 	}
-	
+};
+
+int main()
+{
+	//bool isRunning = true;
+	//std::string userCommand;
+
+	//while (isRunning) {
+	//	std::cout << std::endl << "Enter a command: ";
+	//	getline(std::cin, userCommand);
+
+	//	// Exiting the application
+	//	if (userCommand == std::string("0")) {
+	//		isRunning = false;
+	//	}
+	//}
+	Input i1;
+	i1.readInput();
 }
